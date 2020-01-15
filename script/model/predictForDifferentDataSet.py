75baa3a5297ba22b22e9111128017b320a02dbc7
Rytis <rytis.here@gmail.com>
2020-01-15 17:17:50 +0200

    Script to predict on different data set with all the best weights

--- script/model/predictForDifferentDataSet.py
+++ script/model/predictForDifferentDataSet.py
@@ -1,91 +1,43 @@
-from script.model.utilities import *
-import glob
-from script.model.autoencoder import *
 import os
+import glob
+import shutil
+import cv2
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from script.model.utilities import *
+from script.model.autoencoder import *
 import keras as K
 
-data_gen_args = dict(rotation_range=0.0,
-                    width_shift_range=0.00,
-                    height_shift_range=0.00,
-                    shear_range=0.00,
-                    zoom_range=0.00,
-                    horizontal_flip=False,
-                    fill_mode='nearest')
+def predict_and_save(weight_path, output_path):
+    size = (320, 480, 1)
+    model = AutoEncoder4_5x5(number_of_kernels=32, input_size=size, pretrained_weights=weight_path,
+                             loss_function=Loss.CROSSENTROPY)
+    testGene = testGenerator('D:/Sensors_FromIDAACS/major revision/another dataset comparisson/Images/',
+                             target_size=(320, 480))
+    results = model.predict_generator(testGene, 1233, verbose=1)
+    predictionOutputDir = output_path
+    if not os.path.exists(predictionOutputDir):
+        os.makedirs(predictionOutputDir)
+    saveResult(predictionOutputDir, results)
 
-for setNumber in range(3, 4):
-    configs = ['l4k32AutoEncoder4_5x5_CROSSENTROPY_0_0.001_',
-               'l4k32AutoEncoder4_5x5_CROSSENTROPY25DICE75_0_0.001_',
-               'l4k32AutoEncoder4_5x5_CROSSENTROPY50DICE50_0_0.001_',
-               'l4k32AutoEncoder4_5x5_CROSSENTROPY75DICE25_0_0.001_',
-               'l4k32AutoEncoder4_5x5_DICE_0_0.001_',
-               'l4k32AutoEncoder4_5x5_WEIGHTED60CROSSENTROPY_0_0.001_',
-               'l4k32AutoEncoder4_5x5_WEIGHTED70CROSSENTROPY_0_0.001_',
-               'l4k32AutoEncoder4_5x5_WEIGHTEDCROSSENTROPY_0_0.001_',
-               'l4k32AutoEncoder4_5x5_WEIGHTEDCROSSENTROPY25DICE75_0_0.001_',
-               'l4k32AutoEncoder4_5x5_WEIGHTEDCROSSENTROPY50DICE50_0_0.001_',
-               'l4k32AutoEncoder4_5x5_WEIGHTEDCROSSENTROPY75DICE25_0_0.001_']
+def main():
+    sets = ['Set_0', 'Set_1', 'Set_2', 'Set_3', 'Set_4']
+    for set in sets:
+        architecturesInputDir = 'D:/CrackTrainings_best/' + set + '/'
+        outputDir = 'D:/CrackTrainings_best/' + set + '/'
+        # Get subdirectories of all architectures
+        inputArchitecturesSubDirs = glob.glob(architecturesInputDir + '*/')
+        directory_names = []
+        full_paths = []
+        for inputArchitectureSurDir in inputArchitecturesSubDirs:
+            if 'SEQ' in inputArchitectureSurDir:
+                continue #skip
+            weight_paths = glob.glob(inputArchitectureSurDir + '*.hdf5')
+            output_path = inputArchitectureSurDir + 'crack200/'
+            print(weight_paths[0])
+            #test and save
+            predict_and_save(weight_paths[0], output_path)
 
-    configs = ['l4k32AutoEncoder4_5x5_CROSSENTROPY_0_0.001_']
-    configNumber = 1
-    for config in configs:
-        #configName = 'l5k16Dice_1'
-        configName = config
-        configName += str(setNumber)
-        inputDir = 'C:/src/Set_' + str(setNumber) +'/'+ configName+'/'
-        weightList = glob.glob(inputDir + '*.hdf5')
-        counter = 0
-        for weightPath in weightList:
-            print('Opening: ' + weightPath)
-            fileNameWithExt = weightPath.rsplit('\\', 1)[1]
-            fileName, extension = os.path.splitext(fileNameWithExt)
-            kernels_list = [32]
-            size = (320,480,1)
-            for kernels in kernels_list:
-                if configNumber == 0:
-                    try:
-                        model = AutoEncoder4VGG16_5x5(number_of_kernels = kernels,input_size = size, pretrained_weights = weightPath, loss_function = Loss.CROSSENTROPY)
-                        testGene = testGenerator('E:/RoadCracksInspection/datasets/Set_' + str(setNumber) + '/Test/Images/', target_size = (320,480))
-                        results = model.predict_generator(testGene,35,verbose=1)
-                        predictionOutputDir = 'E:/RoadCracksInspection/trainingOutput/Set_' + str(setNumber) +'/'+configName+'/prediction/' + str(counter) + '/'
-                        if not os.path.exists(predictionOutputDir):
-                            os.makedirs(predictionOutputDir)
-                        saveResult(predictionOutputDir,results)
-                        break
-                    except:
-                        print('Not AutoEncoder4VGG16_5x5')
-                if configNumber == 1:
-                    try:
-                        model = AutoEncoder4_5x5(number_of_kernels = kernels,input_size = size, pretrained_weights = weightPath, loss_function = Loss.CROSSENTROPY)
-                        testGene = testGenerator('D:/RoadCracksInspection/datasets/Set_' + str(setNumber) + '/Test/Images/', target_size = (320,480))
-                        results = model.predict_generator(testGene,35,verbose=1)
-                        predictionOutputDir = 'D:/CracksTrainings/Set_' + str(setNumber) +'/'+configName+'/prediction/' + str(counter) + '/'
-                        if not os.path.exists(predictionOutputDir):
-                            os.makedirs(predictionOutputDir)
-                        saveResult(predictionOutputDir,results)
-                        break
-                    except:
-                        print('Not AutoEncoder4_5x5')
-                if configNumber == 2:
-                    try:
-                        model = AutoEncoder4ResAddOpConcDecFirstEx_5x5(number_of_kernels = kernels,input_size = size, pretrained_weights = weightPath, loss_function = Loss.CROSSENTROPY)
-                        testGene = testGenerator('E:/RoadCracksInspection/datasets/Set_' + str(setNumber) + '/Test/Images/', target_size = (320,480))
-                        results = model.predict_generator(testGene,35,verbose=1)
-                        predictionOutputDir = 'E:/RoadCracksInspection/trainingOutput/Set_' + str(setNumber) +'/'+configName+'/prediction/' + str(counter) + '/'
-                        if not os.path.exists(predictionOutputDir):
-                            os.makedirs(predictionOutputDir)
-                        saveResult(predictionOutputDir,results)
-                        break
-                    except:
-                        print('Not AutoEncoder4ResAddOpConcDecFirstEx_5x5')
-            """    
-            predictionOutputDir = 'E:/RoadCracksInspection/trainingOutputPictures/Set_' + str(setNumber) +'/'+configName+'/prediction/' + str(counter) + '/'
-            if not os.path.exists(predictionOutputDir):
-                os.makedirs(predictionOutputDir)
-            saveResult(predictionOutputDir,results)
-            """
-
-            counter+=1
-            K.backend.clear_session()
-        #configNumber+=1
-            #print('Sleep for 5s !')
-            #time.sleep(5)
+if __name__ == "__main__":
+    main()
