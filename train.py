c5f6d0556c40e863c480f5add7145059bd711a04
Rytis <rytis.here@gmail.com>
2020-04-12 22:14:15 +0300

    Learning rate in model parameters and train.py explained more

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from keras.callbacks import ModelCheckpoint
-from models.autoencoder import UNet4, UNet4_res, UNet4_aspp, UNet4_res_asppWF, UNet4_res_aspp, UNet4_res_asppWF_AG, UNet4_res_aspp_AG
+from models.autoencoder import UNet4, UNet4_res, UNet4_res_asppWF, UNet4_res_aspp, UNet4_res_asppWF_AG, UNet4_res_aspp_AG
 from models.losses import Loss
 from models.utilities import trainGenerator
 import os
@@ -8,20 +8,24 @@ import os
 ###############################
 
 def train():
+    # how many iterations in one epoch? Should cover whole dataset. Divide number of data samples from batch size
+    number_of_iteration = 328
+    # batch size. How many samples you want to feed in one iteration?
+    batch_size = 4
+    # number_of_epoch. How many epoch you want to train?
+    number_of_epoch = 50
 
     # Define model
-    model = UNet4(number_of_kernels=32,input_size = (320,320,1), loss_function = Loss.CROSSENTROPY50DICE50)
-    #model = UNet4_res(number_of_kernels=32,input_size = (320,320,1), loss_function = Loss.CROSSENTROPY50DICE50)
-    #model = UNet4_res_aspp(number_of_kernels=32, input_size=(320, 320, 1), loss_function=Loss.CROSSENTROPY50DICE50)
-    #model = UNet4_res_asppWF(number_of_kernels=32, input_size=(320, 320, 1), loss_function=Loss.CROSSENTROPY50DICE50)
-    #model = UNet4_res_asppWF_AG(number_of_kernels=32, input_size=(320, 320, 1), loss_function=Loss.CROSSENTROPY50DICE50)
-    #model = UNet4_res_aspp_AG(number_of_kernels=32,input_size = (320,320,1), loss_function = Loss.CROSSENTROPY50DICE50)
-    #model = UNet4_aspp(number_of_kernels=32, input_size=(320, 320, 1), loss_function=Loss.CROSSENTROPY50DICE50)
-
+    model = UNet4(number_of_kernels=32,input_size = (320,320,1), loss_function = Loss.CROSSENTROPY50DICE50, learning_rate=1e-3)
+    #model = UNet4_res(number_of_kernels=32,input_size = (320,320,1), loss_function = Loss.CROSSENTROPY50DICE50, learning_rate=1e-3)
+    #model = UNet4_res_aspp(number_of_kernels=32, input_size=(320, 320, 1), loss_function=Loss.CROSSENTROPY50DICE50, learning_rate=1e-3)
+    #model = UNet4_res_asppWF(number_of_kernels=32, input_size=(320, 320, 1), loss_function=Loss.CROSSENTROPY50DICE50, learning_rate=1e-3)
+    #model = UNet4_res_asppWF_AG(number_of_kernels=32, input_size=(320, 320, 1), loss_function=Loss.CROSSENTROPY50DICE50, learning_rate=1e-3)
+    #model = UNet4_res_aspp_AG(number_of_kernels=32,input_size = (320,320,1), loss_function = Loss.CROSSENTROPY50DICE50, learning_rate=1e-3)
     # Where is your data?
     # This path should point to directory with folders 'Images' and 'Labels'
     # In each of mentioned folders should be image and annotations respectively
-    data_dir = 'C:/Users/Rytis/Desktop/CrackForestdatasets_output/'
+    data_dir = r'C:\Users\DeepLearningRig\Desktop\datasets\CrackForestdatasets_output\Train/'
 
     # Possible 'on-the-flight' augmentation parameters
     data_gen_args = dict(rotation_range=0.0,
@@ -32,11 +36,11 @@ def train():
                          horizontal_flip=False,
                          fill_mode='nearest')
 
-    # Define data generator that will take images from directoriy
-    generator = trainGenerator(1, data_dir + 'Train/', 'Images', 'Labels', data_gen_args, save_to_dir = None, target_size = (320,320))
+    # Define data generator that will take images from directory
+    generator = trainGenerator(batch_size, data_dir, 'Images', 'Labels', data_gen_args, save_to_dir = None, target_size = (320,320))
 
     # Directory for weight saving (creates if it does not exist)
-    weights_output_dir = 'C:/Users/Rytis/Desktop/UNet4/'
+    weights_output_dir = 'C:/Users/DeepLearningRig/Desktop/UNet4/'
     if not os.path.exists(weights_output_dir):
         print('Output directory doesnt exist!\n')
         print('It will be created!\n')
@@ -47,7 +51,7 @@ def train():
 
     # Make checkpoint for saving each
     model_checkpoint = ModelCheckpoint(weights_name, monitor='loss',verbose=1, save_best_only=False, save_weights_only=False)
-    model.fit_generator(generator,steps_per_epoch=5,epochs=50,callbacks=[model_checkpoint], shuffle = True)
+    model.fit_generator(generator,steps_per_epoch=number_of_iteration,epochs=number_of_epoch,callbacks=[model_checkpoint], shuffle = True)
 
 def main():
     train()
