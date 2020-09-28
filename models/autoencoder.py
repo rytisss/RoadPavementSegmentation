7295ab02550de12bec1b620d21eafcad77926192
rytis <rytis.augustauskas@ktu.edu>
2020-09-28 08:33:36 +0300

    Few new models

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -125,6 +125,98 @@ def UNet5_First5x5(pretrained_weights=None,
     #plot_model(model, to_file='UNet5_First5x5.png', show_shapes=True, show_layer_names=True)
     return model
 
+def UNet4_First5x5(pretrained_weights=None,
+                 input_size=(320, 320, 1),
+                 kernel_size=3,
+                 number_of_kernels=32,
+                 stride=1,
+                 max_pool=True,
+                 max_pool_size=2,
+                 batch_norm=True,
+                 loss_function=Loss.CROSSENTROPY,
+                useLeakyReLU=True,
+                LeakyReLU_alpha=0.1,
+                 learning_rate = 1e-3):
+    # Input
+    inputs = Input(input_size)
+    # encoding
+    oppositeEnc0, enc0 = EncodingLayer(inputs, number_of_kernels, 5, stride, max_pool, max_pool_size,
+                                       batch_norm, useLeakyReLU=useLeakyReLU, leakyReLU_alpha=LeakyReLU_alpha)
+    oppositeEnc1, enc1 = EncodingLayer(enc0, number_of_kernels * 2, kernel_size, stride, max_pool, max_pool_size,
+                                       batch_norm, useLeakyReLU=useLeakyReLU, leakyReLU_alpha=LeakyReLU_alpha)
+    oppositeEnc2, enc2 = EncodingLayer(enc1, number_of_kernels * 4, kernel_size, stride, max_pool, max_pool_size,
+                                       batch_norm, useLeakyReLU=useLeakyReLU, leakyReLU_alpha=LeakyReLU_alpha)
+    _, enc3 = EncodingLayer(enc2, number_of_kernels * 8, kernel_size, stride, False, max_pool_size,
+                                       batch_norm, useLeakyReLU=useLeakyReLU, leakyReLU_alpha=LeakyReLU_alpha)
+    # decoding
+    # Upsample rate needs to be same as downsampling! It will be equal to the stride and max_pool_size product in opposite (encoding layer)
+    dec2 = DecodingLayer(enc3, oppositeEnc2, 2, number_of_kernels * 4, kernel_size, batch_norm, useLeakyReLU=useLeakyReLU, leakyReLU_alpha=LeakyReLU_alpha)
+    dec1 = DecodingLayer(dec2, oppositeEnc1, 2, number_of_kernels * 2, kernel_size, batch_norm, useLeakyReLU=useLeakyReLU, leakyReLU_alpha=LeakyReLU_alpha)
+    dec0 = DecodingLayer(dec1, oppositeEnc0, 2, number_of_kernels, kernel_size, batch_norm, useLeakyReLU=useLeakyReLU, leakyReLU_alpha=LeakyReLU_alpha)
+
+    dec0 = Conv2D(2, kernel_size=(kernel_size, kernel_size), strides=1, padding='same', kernel_initializer='he_normal')(
+        dec0)
+    if batch_norm:
+        dec0 = BatchNormalization()(dec0)
+
+    dec0 = Activation('relu')(dec0)
+
+    outputs = Conv2D(1, (1, 1), padding="same", activation="sigmoid", kernel_initializer='glorot_normal')(dec0)
+    model = Model(inputs, outputs)
+    # Compile with selected loss function
+    model = CompileModel(model, loss_function, learning_rate)
+    # Load trained weights if they are passed here
+    if pretrained_weights:
+        model.load_weights(pretrained_weights)
+    #plot_model(model, to_file='UNet5_First5x5.png', show_shapes=True, show_layer_names=True)
+    return model
+
+def UNet4_Coordconv_First5x5(pretrained_weights=None,
+                 input_size=(320, 320, 1),
+                 kernel_size=3,
+                 number_of_kernels=32,
+                 stride=1,
+                 max_pool=True,
+                 max_pool_size=2,
+                 batch_norm=True,
+                 loss_function=Loss.CROSSENTROPY,
+                useLeakyReLU=True,
+                LeakyReLU_alpha=0.1,
+                 learning_rate = 1e-3):
+    # Input
+    inputs = Input(input_size)
+    # encoding
+    oppositeEnc0, enc0 = EncodingCoordConvLayer(inputs, number_of_kernels, 5, stride, max_pool, max_pool_size,
+                                       batch_norm, useLeakyReLU=useLeakyReLU, leakyReLU_alpha=LeakyReLU_alpha)
+    oppositeEnc1, enc1 = EncodingCoordConvLayer(enc0, number_of_kernels * 2, kernel_size, stride, max_pool, max_pool_size,
+                                       batch_norm, useLeakyReLU=useLeakyReLU, leakyReLU_alpha=LeakyReLU_alpha)
+    oppositeEnc2, enc2 = EncodingCoordConvLayer(enc1, number_of_kernels * 4, kernel_size, stride, max_pool, max_pool_size,
+                                       batch_norm, useLeakyReLU=useLeakyReLU, leakyReLU_alpha=LeakyReLU_alpha)
+    _, enc3 = EncodingCoordConvLayer(enc2, number_of_kernels * 8, kernel_size, stride, False, max_pool_size,
+                                       batch_norm, useLeakyReLU=useLeakyReLU, leakyReLU_alpha=LeakyReLU_alpha)
+    # decoding
+    # Upsample rate needs to be same as downsampling! It will be equal to the stride and max_pool_size product in opposite (encoding layer)
+    dec2 = DecodingLayerCoordConv(enc3, oppositeEnc2, 2, number_of_kernels * 4, kernel_size, batch_norm, useLeakyReLU=useLeakyReLU, leakyReLU_alpha=LeakyReLU_alpha)
+    dec1 = DecodingLayerCoordConv(dec2, oppositeEnc1, 2, number_of_kernels * 2, kernel_size, batch_norm, useLeakyReLU=useLeakyReLU, leakyReLU_alpha=LeakyReLU_alpha)
+    dec0 = DecodingLayerCoordConv(dec1, oppositeEnc0, 2, number_of_kernels, kernel_size, batch_norm, useLeakyReLU=useLeakyReLU, leakyReLU_alpha=LeakyReLU_alpha)
+
+    dec0 = Conv2D(2, kernel_size=(kernel_size, kernel_size), strides=1, padding='same', kernel_initializer='he_normal')(
+        dec0)
+    if batch_norm:
+        dec0 = BatchNormalization()(dec0)
+
+    dec0 = Activation('relu')(dec0)
+
+    outputs = Conv2D(1, (1, 1), padding="same", activation="sigmoid", kernel_initializer='glorot_normal')(dec0)
+    model = Model(inputs, outputs)
+    # Compile with selected loss function
+    model = CompileModel(model, loss_function, learning_rate)
+    # Load trained weights if they are passed here
+    if pretrained_weights:
+        model.load_weights(pretrained_weights)
+    #plot_model(model, to_file='UNet5_First5x5.png', show_shapes=True, show_layer_names=True)
+    return model
+
 def UNet4_res(pretrained_weights=None,
                                 input_size=(320, 320, 1),
                                 kernel_size=3,
@@ -660,7 +752,7 @@ def UNet4_res_aspp_First5x5(pretrained_weights=None,
         dec0)
     if batch_norm:
         dec0 = BatchNormalization()(dec0)
-    dec0 = LeakyReLU(alpha=LeakyReLU_alpha)(dec0) if useLeakyReLU else Activation('relu')(dec0)
+    dec0 = Activation('relu')(dec0)
 
     outputs = Conv2D(1, (1, 1), padding="same", activation="sigmoid", kernel_initializer='glorot_normal')(dec0)
     model = Model(inputs, outputs)
@@ -705,6 +797,8 @@ def UNet4_res_aspp_First5x5_CoordConv(pretrained_weights=None,
     dec1 = DecodingCoordConvLayerRes(dec2, oppositeEnc1, 2, number_of_kernels * 2, kernel_size, batch_norm, useLeakyReLU, LeakyReLU_alpha)
     dec0 = DecodingCoordConvLayerRes(dec1, oppositeEnc0, 2, number_of_kernels, kernel_size, batch_norm, useLeakyReLU, LeakyReLU_alpha)
 
+    dec0 = Conv2D(2, kernel_size=(kernel_size, kernel_size), strides=1, padding='same', kernel_initializer='he_normal')(
+        dec0)
     if batch_norm:
         dec0 = BatchNormalization()(dec0)
 
